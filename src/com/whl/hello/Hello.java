f7c8b782c6e9663ed21b1db7723de8a06ba309fe
whlin <whlin@whl>
2017-03-28 20:45:33 +0800

    创建hello

--- src/com/whl/hello/Hello.java
+++ src/com/whl/hello/Hello.java
@@ -1,9 +1,10 @@
 package com.whl.hello;
 
-public class App {
+public class Hello {
 
 	public static void main(String[] args) {
 
-		System.out.println("hello world!");
+		System.out.println("»¶Ó­£¡");
 	}
+
 }
